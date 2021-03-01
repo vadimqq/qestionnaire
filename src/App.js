029d5a71669284691e90430582d4362d203b96f3
Vadim <66322242+vadimqq@users.noreply.github.com>
2021-03-01 14:59:58 +0400

    create DataProvider and test route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+const Step1 = () => <> Step 1 </>
+const Step2 = () => <> Step 2 </>
+const Step3 = () => <> Step 3 </>
+const Result = () => <> result </>
+
+const Header = () => <h1>Header</h1>
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Router>
+    <Switch>
+      <Route exact path="/" component={Step1} />
+      <Route path="/step2" component={Step2} />
+      <Route path="/step3" component={Step3} />
+      <Route path="/step4" component={Result} />
+    </Switch>
+    </Router>
+    </>
   );
 }
 
